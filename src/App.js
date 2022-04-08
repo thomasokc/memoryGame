075f25d1d3f8fe06a094d0e9abaa340eac83d34a
Tom Morris <thomasmorris682@gmail.com>
2022-04-08 14:30:00 -0500

    final product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { "src": "/img/cow.jpg" },
-  { "src": "/img/giraffe.jpg" },
-  { "src": "/img/monkey.jpg" },
-  { "src": "/img/polarBear.jpg" },
-  { "src": "/img/pug.jpg" },
-  { "src": "/img/panda.jpg" }, 
+  { "src": "/img/cow.jpg", matched: false },
+  { "src": "/img/giraffe.jpg", matched: false },
+  { "src": "/img/monkey.jpg", matched: false },
+  { "src": "/img/polarBear.jpg", matched: false },
+  { "src": "/img/pug.jpg", matched: false },
+  { "src": "/img/panda.jpg", matched: false }, 
 ]
 
 function App() {
@@ -16,35 +16,82 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
 
-  // shuffle cards
+  // shuffle cards for new game
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
-    .sort(() => Math.random() - 0.5)
-    .map((card) => ({ ...card, id: Math.random() }))
-
+      .sort(() => Math.random() - 0.5)
+      .map(card => ({ ...card, id: Math.random() }))
+      
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards)
     setTurns(0)
   }
 
   // handle a choice
-  const handleChoice = (card) => { //passing this function to the single card component
-    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)                      
-  }                                 
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+
+  // Auto start game
+  useEffect(() => {
+    shuffleCards()
+  }, [])
 
   return (
     <div className="App">
       <h1>Animal Match</h1>
       <button onClick={shuffleCards}>New Game</button>
 
-      <div className="card-grid"> 
+      <div className="card-grid">
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+          <SingleCard 
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped = {card == choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
-
+      <p>Turns: {turns}</p>
     </div>
   );
 }
 
-export default App;
+export default App
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,14 +1,16 @@
 import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
 
     const handleClick = () => {
-        handleChoice(card)
+        if (!disabled) {
+            handleChoice(card)
+        }
     } 
 
     return (
         <div className="card">
-            <div>
+            <div className={flipped ? "flipped" : ""}>
                 <img className='front' src={card.src} alt="card front"></img>
                 <img className='back' alt="card back" src="/img/earth.jpg" onClick={handleClick}></img>
             </div>
