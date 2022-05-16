4284e6ebe636f7d3e75d858d993bd58de6b0747b
Thomas Morris <thomasmorris682@gmail.com>
2022-05-16 14:48:02 -0500

    played with firebase hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC57N9wSf3zFxIdqo2Nn66IqAqBxicwj1Y",
+    authDomain: "memorygame-3a63c.firebaseapp.com",
+    projectId: "memorygame-3a63c",
+    storageBucket: "memorygame-3a63c.appspot.com",
+    messagingSenderId: "887641501954",
+    appId: "1:887641501954:web:944a77f583bf1f0399921b"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
